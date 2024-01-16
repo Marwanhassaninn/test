251b076066a5ff4f311304c9be4b36591b5b5a8c
Marwan hassanin <114306902+Marwanhassaninn@users.noreply.github.com>
2024-01-16 21:13:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// function toggleMenu() {
+//     var navLinks = document.getElementById("navLinks");
+//     navLinks.classList.toggle("show");
+// }
+
+function toggleMenu() {
+    var navLinks = document.getElementById("navLinks");
+    navLinks.classList.toggle("show");
+}
+
+var links = document.querySelectorAll(".nav-links a");
+links.forEach(function(link) {
+    link.addEventListener("click", function() {
+        var navLinks = document.getElementById("navLinks");
+        navLinks.classList.remove("show");
+    });
+});
+
+   
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Initialize lightGallery on all elements with the 'btn-view' class
+    document.querySelectorAll('.btn-view').forEach(function (element) {
+        element.addEventListener('click', function () {
+            lightGallery(element, {
+                dynamic: true,
+                dynamicEl: [{
+                    src: element.getAttribute('data-src'),
+                }],
+            });
+        });
+    });
+});
+
+
